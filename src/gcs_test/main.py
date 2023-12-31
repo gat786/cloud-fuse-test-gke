d1724827a7e1e095d5d1142c09e35777334e9cdc
Ganesh Tiwari <ganesht049@gmail.com>
2023-12-31 11:37:46 +0530

    feat: added code to read and write filees

--- src/gcs_test/main.py
+++ src/gcs_test/main.py
@@ -1,2 +1,18 @@
+import os
+from datetime import datetime
+
 def main():
   print("Hello, World!")
+
+def read_files(dir_to_read: str):
+  files = os.listdir(dir_to_read)
+  for file in files:  
+    print(file)
+    
+def write_file(dir_to_write: str):
+  now = datetime.now()
+  
+  file_name = now.strftime("%Y%m%d+%H%M%S")
+  with open(f"{dir_to_write}/{file_name}.txt", "w") as f:
+    f.write(f"Hello, World! at {file_name}")
+  
